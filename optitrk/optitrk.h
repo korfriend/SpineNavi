c8375e5514de4e45a6cc4a02998479ee83190588
동준 김 <korfriend@gmail.com>
2023-09-22 13:33:08 +0900

    add pivot interface

--- optitrk/optitrk.h
+++ optitrk/optitrk.h
@@ -35,6 +35,10 @@ namespace optitrk
 	__dojostatic bool SetRigidBodyEnabledbyName(const std::string& name, const bool enabled);
 
 	__dojostatic bool SetRigidBody(const std::string& rbName, const int numMKs, const float* mkPosArray);
+
+	__dojostatic bool StartPivotSample(const std::bitset<128>& cidRb, const int numSamples); 
+	__dojostatic bool ProcessPivotSample(float* progress, float* errInit, float* errResult); // a percentage of the total samples during sampling.
+
 	// mat_cam2ws ==> glm::fmat4x4
 	// normal camera space.. up : y down (-y), view : +z
 	__dojostatic bool GetCameraLocation(const int cam_idx, float* mat_cam2ws);
